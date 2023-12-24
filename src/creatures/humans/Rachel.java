5d790649376de0007734e2bea506345f0f5035f9
Nika <nottnishi@yandex.ru>
2023-12-25 00:34:02 +0300

    Lab 4 first try

--- src/creatures/humans/Rachel.java
+++ src/creatures/humans/Rachel.java
@@ -4,14 +4,26 @@ import creatures.humans.moves.Speakable;
 import exceptions.EmptySpeechException;
 
 public class Rachel extends Human implements Speakable {
+
+    public boolean isSick() {
+        return isSick;
+    }
+
+    public void setSick(boolean sick) {
+        isSick = sick;
+    }
+
+    private boolean isSick;
     public Rachel(String name, int age, int fatigue, int surprise) {
         super(name, age, fatigue, surprise);
+        this.isSick = false;
     }
 
     @Override
     public void speak(String phrase) throws EmptySpeechException {
         if (phrase != null && !phrase.isEmpty()) {
             System.out.println(name + " говорит: " + phrase);
+            if (phrase == ""
         } else {
             throw new EmptySpeechException(name);
         }
